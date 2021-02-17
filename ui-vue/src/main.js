f3c6d6de0103b0326fb26b5ef1bd822386181de1
hveldstra <h@veldstra.org>
2021-02-17 10:58:50 +0000

    Add Vue PoC

---
+++ ui-vue/src/main.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import './assets/styles/index.css';
+
+const {sortBy} = require('lodash');
+
+Vue.config.productionTip = false
+
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+Vue.use(Loading);
+
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/s',
+    component: App
+  }
+];
+const router = new VueRouter({routes});
+
+const app = new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
+
+const spinner = app.$loading.show();
+let ENDPOINT = '';
+let ws = new WebSocket(ENDPOINT);
+window.ws = ws;
+
+// TODO: Handle reconnect
+
+ws.onopen = function(event) {
+  console.log(event);
+  console.log('WS open');
+  spinner.hide();
+}
+
+ws.onmessage = function(event) {
+  console.log(event);
+
+  const eventData = JSON.parse(event.data);
+  if(eventData.event === 'output') {
+    let newItems = app.$root.$children[0].items.concat(eventData);
+
+    app.$root.$children[0].items = sortBy(newItems, x => x.ts);
+
+  } else if (eventData.event === 'done') {
+    // TODO: Re-enable Run button
+    console.log('Run done');
+  }
+}
