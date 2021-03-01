ec51ce2a56094d9705eed1a7392a0bea6a7e5a51
Francesco Lentini <francesco.lentini@gmail.com>
2021-03-01 18:37:25 +0000

    Add save/get scenarios HTTP endpoints

--- lambda-runner/handler.js
+++ lambda-runner/handler.js
@@ -1,26 +1,26 @@
-'use strict';
+"use strict";
 
-const AWS = require('aws-sdk');
-const util = require('util');
-const tmp = require('tmp');
-const EventEmitter = require('events');
-const path = require('path');
-const atob = require('atob');
-const { writeFileSync } = require('fs');
-const childProcess = require('child_process');
-const Yaml = require('js-yaml');
+const AWS = require("aws-sdk");
+const util = require("util");
+const tmp = require("tmp");
+const EventEmitter = require("events");
+const path = require("path");
+const atob = require("atob");
+const { writeFileSync } = require("fs");
+const childProcess = require("child_process");
+const Yaml = require("js-yaml");
 
-module.exports.connect = async(event) => {
+module.exports.connect = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify('ok')
+    body: JSON.stringify("ok"),
   };
 };
 
-module.exports.disconnect = async(event) => {
+module.exports.disconnect = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify('ok')
+    body: JSON.stringify("ok"),
   };
 };
 
@@ -28,7 +28,7 @@ const sendMessageToClient = (url, connectionId, payload) => {
   scheduled++;
   return new Promise((resolve, reject) => {
     const apigatewaymanagementapi = new AWS.ApiGatewayManagementApi({
-      apiVersion: '2018-11-29',
+      apiVersion: "2018-11-29",
       endpoint: url,
     });
     apigatewaymanagementapi.postToConnection(
@@ -46,15 +46,15 @@ const sendMessageToClient = (url, connectionId, payload) => {
       }
     );
   });
-}
+};
 
 const sanitize = (inputScript) => {
   return inputScript;
-}
+};
 
 const addOverrides = (inputScript) => {
   return inputScript;
-}
+};
 
 const runArtillery = (script, opts, events) => {
   let child;
@@ -66,45 +66,49 @@ const runArtillery = (script, opts, events) => {
 
     try {
       doc = Yaml.load(script);
-    } catch(err) {
+    } catch (err) {
       console.log(err);
       doc = {};
     }
 
     doc.config.phases = [
-        {
-          "duration": 1,
-          "arrivalCount": 1
-        }
-      ];
+      {
+        duration: 1,
+        arrivalCount: 1,
+      },
+    ];
 
-    doc.config.plugins = {"expect":{}};
-  
-    args.push('run');
-    args.push('-q');
+    doc.config.plugins = { expect: {} };
+
+    args.push("run");
+    args.push("-q");
     args.push(tempFile.name);
-
 
     writeFileSync(tempFile.name, JSON.stringify(doc));
 
-    child = childProcess.spawn('node', [path.join(__dirname, 'node_modules', 'artillery', 'bin', 'artillery')].concat(args), {});
+    child = childProcess.spawn(
+      "node",
+      [
+        path.join(__dirname, "node_modules", "artillery", "bin", "artillery"),
+      ].concat(args),
+      {}
+    );
 
-    child.on('close', (code) => {
-      events.emit('done', code);
+    child.on("close", (code) => {
+      events.emit("done", code);
       resolve();
     });
 
     if (child.stdout) {
-      child.stdout.on('data', (output) => {
-        events.emit('output', output.toString());
+      child.stdout.on("data", (output) => {
+        events.emit("output", output.toString());
       });
-      child.stderr.on('data', (output) => {
-        events.emit('output', output.toString());
+      child.stderr.on("data", (output) => {
+        events.emit("output", output.toString());
       });
-      child.stdout.on('end', () => {
-      });
+      child.stdout.on("end", () => {});
     } else {
-      reject(new Error('no stdout'));
+      reject(new Error("no stdout"));
     }
   });
 };
@@ -114,7 +118,7 @@ let scheduled = 0;
 
 function flush() {
   return new Promise((resolve, reject) => {
-    let i = setInterval(function() {
+    let i = setInterval(function () {
       if (scheduled == 0) {
         clearInterval(i);
         resolve();
@@ -133,16 +137,24 @@ module.exports.wsRun = async (event, context) => {
   const domain = event.requestContext.domainName;
   const stage = event.requestContext.stage;
   const connectionId = event.requestContext.connectionId;
-  const callbackUrlForAWS = util.format(util.format('https://%s/%s', domain, stage));
-  
+  const callbackUrlForAWS = util.format(
+    util.format("https://%s/%s", domain, stage)
+  );
+
   const events = new EventEmitter();
 
-  events.on('output', async (s) => {
-    await sendMessageToClient(callbackUrlForAWS, connectionId, {event: 'output', data: s, ts: Date.now()});  
+  events.on("output", async (s) => {
+    await sendMessageToClient(callbackUrlForAWS, connectionId, {
+      event: "output",
+      data: s,
+      ts: Date.now(),
+    });
   });
 
-  events.on('done', async () => {    
-    await sendMessageToClient(callbackUrlForAWS, connectionId, {event: 'done'});
+  events.on("done", async () => {
+    await sendMessageToClient(callbackUrlForAWS, connectionId, {
+      event: "done",
+    });
   });
 
   await runArtillery(runnableScript, {}, events);
@@ -151,4 +163,88 @@ module.exports.wsRun = async (event, context) => {
   return {
     statusCode: 200,
   };
-}
+};
+
+const scenarioItem = (scenario, output) => {
+  return {
+    id: uuid.v4(),
+    scenario,
+    output,
+    submittedAt: new Date().getTime(),
+  };
+};
+
+module.exports.saveScenario = async (event) => {
+  try {
+    const { scenario, output } = JSON.parse(event.body);
+    const data = scenarioItem(scenario, output);
+    const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+    await dynamoDb
+      .put({
+        TableName: process.env.SCENARIOS_TABLE,
+        Item: data,
+      })
+      .promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        key: data.id,
+      }),
+    };
+  } catch (err) {
+    console.error(err);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        err,
+      }),
+    };
+  }
+};
+
+module.exports.getScenario = async (event) => {
+  try {
+    const key = event.pathParameters.key;
+
+    if (!key) {
+      return {
+        statusCode: 400,
+      };
+    }
+
+    const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+    const { Item: item } = await dynamoDb
+      .get({
+        TableName: process.env.SCENARIOS_TABLE,
+        Key: {
+          id: key,
+        },
+      })
+      .promise();
+
+    if (!item) {
+      return {
+        statusCode: 404,
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(item),
+    };
+  } catch (err) {
+    console.error(err);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        err,
+      }),
+    };
+  }
+};
+
