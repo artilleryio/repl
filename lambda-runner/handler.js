cadfbf23bf2005d4974fc713fe1fbf41ad12e33d
Francesco Lentini <francesco.lentini@gmail.com>
2021-03-04 18:51:18 +0000

    chore: lint lambda-runner/handler

--- lambda-runner/handler.js
+++ lambda-runner/handler.js
@@ -1,35 +1,36 @@
-"use strict";
+const AWS = require('aws-sdk');
+const util = require('util');
+const tmp = require('tmp');
+const EventEmitter = require('events');
+const path = require('path');
+const atob = require('atob');
+const { writeFileSync } = require('fs');
+const childProcess = require('child_process');
+const Yaml = require('js-yaml');
+const uuid = require('uuid');
 
-const AWS = require("aws-sdk");
-const util = require("util");
-const tmp = require("tmp");
-const EventEmitter = require("events");
-const path = require("path");
-const atob = require("atob");
-const { writeFileSync } = require("fs");
-const childProcess = require("child_process");
-const Yaml = require("js-yaml");
-const uuid = require("uuid");
+let scheduled = 0;
 
-module.exports.connect = async (event) => {
+module.exports.connect = async () => {
   return {
     statusCode: 200,
-    body: JSON.stringify("ok"),
+    body: JSON.stringify('ok'),
   };
 };
 
-module.exports.disconnect = async (event) => {
+module.exports.disconnect = async () => {
   return {
     statusCode: 200,
-    body: JSON.stringify("ok"),
+    body: JSON.stringify('ok'),
   };
 };
 
 const sendMessageToClient = (url, connectionId, payload) => {
-  scheduled++;
+  scheduled += 1;
+
   return new Promise((resolve, reject) => {
     const apigatewaymanagementapi = new AWS.ApiGatewayManagementApi({
-      apiVersion: "2018-11-29",
+      apiVersion: '2018-11-29',
       endpoint: url,
     });
     apigatewaymanagementapi.postToConnection(
@@ -39,12 +40,12 @@ const sendMessageToClient = (url, connectionId, payload) => {
       },
       (err, data) => {
         if (err) {
-          scheduled--;
+          scheduled -= 1;
           reject(err);
         }
-        scheduled--;
+        scheduled -= 1;
         resolve(data);
-      }
+      },
     );
   });
 };
@@ -63,7 +64,7 @@ const runArtillery = (script, opts, events) => {
     const tempFile = tmp.fileSync();
 
     let doc = {};
-    let args = [];
+    const args = [];
 
     try {
       doc = Yaml.load(script);
@@ -81,46 +82,44 @@ const runArtillery = (script, opts, events) => {
 
     doc.config.plugins = { expect: {} };
 
-    args.push("run");
-    args.push("-q");
+    args.push('run');
+    args.push('-q');
     args.push(tempFile.name);
 
     writeFileSync(tempFile.name, JSON.stringify(doc));
 
     child = childProcess.spawn(
-      "node",
+      'node',
       [
-        path.join(__dirname, "node_modules", "artillery", "bin", "artillery"),
+        path.join(__dirname, 'node_modules', 'artillery', 'bin', 'artillery'),
       ].concat(args),
-      {}
+      {},
     );
 
-    child.on("close", (code) => {
-      events.emit("done", code);
+    child.on('close', (code) => {
+      events.emit('done', code);
       resolve();
     });
 
     if (child.stdout) {
-      child.stdout.on("data", (output) => {
-        events.emit("output", output.toString());
+      child.stdout.on('data', (output) => {
+        events.emit('output', output.toString());
       });
-      child.stderr.on("data", (output) => {
-        events.emit("output", output.toString());
+      child.stderr.on('data', (output) => {
+        events.emit('output', output.toString());
       });
-      child.stdout.on("end", () => {});
+      child.stdout.on('end', () => {});
     } else {
-      reject(new Error("no stdout"));
+      reject(new Error('no stdout'));
     }
   });
 };
 module.exports.runArtillery = runArtillery;
 
-let scheduled = 0;
-
 function flush() {
-  return new Promise((resolve, reject) => {
-    let i = setInterval(function () {
-      if (scheduled == 0) {
+  return new Promise((resolve) => {
+    const i = setInterval(() => {
+      if (scheduled === 0) {
         clearInterval(i);
         resolve();
       }
@@ -129,32 +128,31 @@ function flush() {
 }
 
 // default route for all WS messages
-module.exports.wsRun = async (event, context) => {
-  let { input, opts } = JSON.parse(event.body);
+module.exports.wsRun = async (event) => {
+  let { input } = JSON.parse(event.body);
   input = atob(input);
   // TODO: Parse in here
   const runnableScript = addOverrides(sanitize(input));
 
   const domain = event.requestContext.domainName;
-  const stage = event.requestContext.stage;
-  const connectionId = event.requestContext.connectionId;
+  const { stage, connectionId } = event.requestContext;
   const callbackUrlForAWS = util.format(
-    util.format("https://%s/%s", domain, stage)
+    util.format('https://%s/%s', domain, stage),
   );
 
   const events = new EventEmitter();
 
-  events.on("output", async (s) => {
+  events.on('output', async (s) => {
     await sendMessageToClient(callbackUrlForAWS, connectionId, {
-      event: "output",
+      event: 'output',
       data: s,
       ts: Date.now(),
     });
   });
 
-  events.on("done", async () => {
+  events.on('done', async () => {
     await sendMessageToClient(callbackUrlForAWS, connectionId, {
-      event: "done",
+      event: 'done',
     });
   });
 
@@ -208,7 +206,7 @@ module.exports.saveScenario = async (event) => {
 
 module.exports.getScenario = async (event) => {
   try {
-    const key = event.pathParameters.key;
+    const { key } = event.pathParameters;
 
     if (!key) {
       return {
