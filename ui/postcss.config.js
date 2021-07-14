feee2dd6d6969c6bd6f4819cb5884436ce3c0611
Francesco Lentini <francesco.lentini@gmail.com>
2021-07-14 16:33:59 +0100

    replace vue ui with nextjs

--- ui/postcss.config.js
+++ ui/postcss.config.js
@@ -1,10 +1,8 @@
-// postcss.config.js
-const autoprefixer = require('autoprefixer');
-const tailwindcss = require('tailwindcss');
-
+// If you want to use other PostCSS plugins, see the following:
+// https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-  ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
