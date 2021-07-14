38745422f51abed4ff0fabbebad245772e46e833
Francesco Lentini <francesco.lentini@gmail.com>
2021-07-14 11:04:33 +0100

    clean up

--- nextjs-ui/components/ui/Avatar.js
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image';
-
-const Avatar = ({ img, alt }) => (
-	<div className="app-avatar bg-gray-light rounded-full overflow-hidden h-9 w-9">
-		<Image src={ img } alt={ alt } width={36} height={36} />
-	</div>
-);
-
-export default Avatar;
